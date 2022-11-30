3a23968c382b29a54c456babb06d5f9a0fc1bd07
faizabdullah0134 <faizahf0134@gmail.com>
2022-11-30 18:12:19 +0700

    pertemuan1

---
+++ pertemuan1/for.py
@@ -0,0 +1,13 @@
+for i in range(10, 15, 2): # nilai pertama: start, kedua: stop, ketiga: selang
+    print(i, '-- Hello --')
+    for j in range(3):
+        print(j, '-- olla --')
+print('Hai')
+
+total = 0
+for k in range(1, 6):
+    total = total + k
+print(total)
+
+contoh = 6 - 4 **2
+print(contoh)
---
+++ pertemuan1/percabangan.py
@@ -0,0 +1,8 @@
+num = eval(input('Masukkan Angka: '))
+if num > 0:
+    print("Bilangan positif")
+elif num < 0:
+    print("Bilangan negatif")
+else:
+    print("Bilangan 0")
+print("Selesai")
