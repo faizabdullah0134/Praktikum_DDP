9cbae6bfba1896374cde6b623d3b66cb50c1ffa3
faizabdullah0134 <faizahf0134@gmail.com>
2022-12-28 18:32:24 +0700

    pertemuan3

---
+++ pertemuan3/operator.py
@@ -0,0 +1,28 @@
+number1 = 5
+number2 = 10
+
+# operator perbandingan
+print(number1 != number2)
+
+if number1 != number2:
+    print("Nomor berbeda!")
+else:
+    print("Nomor sama!")
+
+# Operator logika
+if number1 > 99 and number1 < 1000:
+    print("Bilangan ratusan!")
+else:
+    print("Bukan bilangan ratusan!")
+
+# Operator aritmatika
+while True:
+    number3 = int(input("Masukkan angka: "))
+
+    if number3 == 00:
+        break
+
+    if number3 % 2 == 0:
+        print("Bilangan genap")
+    else:
+        print("Bilangan ganjil")
